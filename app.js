ce79eeb4e228f53edd59540d2b900f06c6254d33
Alexander Udalov <udalov.alexander@gmail.com>
2012-07-02 19:20:56 +0400

    submissions

--- app.js
+++ app.js
@@ -66,11 +66,11 @@ app.configure('production', function(){
 everyauth.helpExpress(app);
 
 
-var route = (function(title) {
-  return function(name) {
-    return function(req, res) { res.render(name, { title: title }); }
-  }
-})('Settlers Online');
+var title = 'Settlers Online';
+
+var route = function(name) {
+  return function(req, res) { res.render(name, { title: title }); }
+};
 
 app.get('/', route('index'));
 app.get('/docs', route('docs'));
@@ -78,7 +78,9 @@ app.get('/download', route('download'));
 app.get('/login', route('login'));
 app.get('/submit', function(req, res) {
   if (!req.user) return res.redirect('/login');
-  route('submit')(req, res);
+  Submission.find({ author: req.user.id }, function(err, submissions) {
+    res.render('submit', { title: title, submissions: submissions });
+  });
 });
 app.post('/submit', function(req, res) {
   req.method = 'get';
