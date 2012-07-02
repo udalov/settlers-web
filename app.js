3eeeef2f4b828c0c913c0f9654e77db750958814
Alexander Udalov <udalov.alexander@gmail.com>
2012-07-02 20:06:47 +0400

    move routing to routes.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var config = require('./config');
-var fs = require('fs');
 var everyauth = require('everyauth');
 var express = require('express');
 var MongoStore = require('connect-mongo')(express);
-var db = require('./db');
-var strftime = require('prettydate').strftime;
 
 var app = module.exports = express.createServer();
 
+var db = require('./db');
 var User = db.User;
-var Submission = db.Submission;
-var Solution = db.Solution;
 
 everyauth.facebook
   .appId(config.appId)
@@ -66,52 +62,7 @@ app.configure('production', function(){
 
 everyauth.helpExpress(app);
 
-
-var title = 'Settlers Online';
-
-var route = function(name) {
-  return function(req, res) { res.render(name, { title: title }); }
-};
-
-app.get('/', route('index'));
-app.get('/docs', route('docs'));
-app.get('/download', route('download'));
-app.get('/login', route('login'));
-app.get('/submit', function(req, res) {
-  if (!req.user) return res.redirect('/login');
-  Submission.find({ author: req.user.id }, function(err, submissions) {
-    res.render('submit', {
-      title: title,
-      submissions: submissions.reverse(),
-      strftime: strftime
-    });
-  });
-});
-app.post('/submit', function(req, res) {
-  req.method = 'get';
-  if (!req.user) return res.redirect('/login');
-  res.redirect('/submit');
-  if (!req.files || !req.files.file || !req.files.file.size) return;
-  var file = req.files.file;
-  fs.readFile(file.path, function(err, code) {
-    if (err) throw err;
-    if (!code) return;
-    var solution = new Solution({
-      filename: file.name,
-      code: new Buffer(code)
-    });
-    solution.save(function(err) {
-      if (err) throw err;
-      var submission = new Submission({
-        author: req.user._id,
-        solution: solution._id
-      });
-      submission.save(function(err) {
-        if (err) throw err;
-      });
-    });
-  });
-});
+require('./routes')(app);
 
 app.listen(3010, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
